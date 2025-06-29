153566f7cc5c9ddaba980ccc66b328b4a2772207
amirkhan <amirkhanmags@gmail.com>
2025-06-29 14:58:54 +0300

    feat: реализована авторизация с контекстом и защищёнными маршрутами

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,6 +3,8 @@ import type { ReactNode } from 'react';
 
 import { AuthContext } from './useAuth';
 
+// создание провайдера
+
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(() => {
     return localStorage.getItem('isAuthenticated') === 'true';
--- src/hoc/PrivateRoute.tsx
+++ src/hoc/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/useAuth';
 import type { JSX } from 'react';
 
-//
+// создание приватных страницы
 
 export const PrivateRoute = ({ children }: { children: JSX.Element }) => {
   const { isAuthenticated } = useAuth();
